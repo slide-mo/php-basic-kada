4d56d30483f51cb7f395e094360d4746c9dbf7d6
slide-mo <151592815+slide-mo@users.noreply.github.com>
2024-01-04 17:44:11 +0900

    修正ファイルを追加

---
+++ kadai_sort/kadai_sort/sort.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        function sort_2way($nums, $order) {
+            if ($order === 'ascending_order') {
+                echo '昇順にソートします。<br>';
+                sort($nums);
+                foreach ($nums as $val) {
+                    echo "$val<br>";
+                }
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($nums);
+                foreach ($nums as $val) {           
+                    echo "$val<br>";
+                }
+            }
+        }
+        echo sort_2way($nums, 'ascending_order');
+        echo sort_2way($nums, 'descending_order');
+        ?>
+    </p>
+</body>
+</html>
